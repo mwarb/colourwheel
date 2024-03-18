013409f37befbdb57bd6fc55fb9685e1d7b7e923
Matthew Warburton <40635598+mwarb@users.noreply.github.com>
2024-03-18 16:37:07 +0000

    updated jitters

--- custom-plugins/stimuli-display.js
+++ custom-plugins/stimuli-display.js
@@ -25,8 +25,8 @@ var stimuliDisplay = {
         this.colorwheelOuterDiameter = pxPerMillimeter * 160;
 
         // jitter bounds
-        this.jitterWidth = pxPerMillimeter * 10;
-        this.jitterHeight = pxPerMillimeter * 10;
+        this.jitterWidth = pxPerMillimeter * 8;
+        this.jitterHeight = pxPerMillimeter * 8;
     },
 
     getLocationsFromAngles(locationAngles) {
